6f55c8ca910327ed62caec7283e822f3fd42efa2
Alex Jiang <alex.jiang@google.com>
2018-03-22 17:06:35 +1100

    [week4](import): demo boxing

---
+++ W4/src/BoxingDemo.java
@@ -0,0 +1,13 @@
+public class BoxingDemo {
+    public static void main(String[] args) {
+        // Example 1: equality operator mixing object and primitive
+        Integer num1 = 1; // autoboxing
+        int num2 = 1;
+        System.out.println("num1 == num2 : " + (num1 == num2));
+
+        // Example 2: equality operator - pure object comparison
+        Integer one = new Integer(1); // no autoboxing
+        Integer anotherOne = new Integer(1);
+        System.out.println("one == anotherOne : " + (one == anotherOne));
+    }
+}
