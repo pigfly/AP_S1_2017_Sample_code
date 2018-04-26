5fe739a58327774df1773d523ecf82722b56bb44
Alex Jiang <pigflyjky@gmail.com>
2018-04-26 16:03:40 +1000

    [week 8](type erase): sample import

---
+++ W8/src/TypeErase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class TypeErase {
+    public static void main(String[] args) {
+        Class c1 = new ArrayList<String>().getClass();
+        Class c2 = new ArrayList<Integer>().getClass();
+        System.out.println(c1 == c2);
+    }
+}
