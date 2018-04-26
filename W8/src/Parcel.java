e8f8e336df2832f01f3218da342635181e62d6a5
Alex Jiang <pigflyjky@gmail.com>
2018-04-26 16:20:37 +1000

    [week 8](inner class): sample import

---
+++ W8/src/Parcel.java
@@ -0,0 +1,23 @@
+public class Parcel {
+    class Contents {
+        private int i = 11;
+        public int value() { return i; }
+    }
+    class Destination {
+        private String label;
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+        String readLabel() { return label; }
+    }
+
+    public void ship(String dest) {
+        Contents c = new Contents();
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+    public static void main(String[] args) {
+        Parcel p = new Parcel();
+        p.ship("Melbourne");
+    }
+}
